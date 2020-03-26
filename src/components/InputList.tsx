7d16d47b760ad60c0fcfc7fa2eaa691ea61a5351
lioness3 <jcnailtech3@yahoo.com>
2020-03-26 15:53:04 -0700

    add Input List component

---
+++ src/components/InputList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './InputList.css'
+
+interface ContainerProps {
+
+}
+
+const InputList: React.FC<ContainerProps> = () => {
+  return (
+    <div className="">
+      <div className="header">
+        <h1>Add your needed items below:</h1>
+      </div>
+
+      <div className="">
+
+
+      </div>
+    </div>
+  );
+};
+
+export default InputList;
