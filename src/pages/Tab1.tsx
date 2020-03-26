5b1c15146c93ce21972366916f894857c1db4b0d
lioness3 <jcnailtech3@yahoo.com>
2020-03-26 16:12:22 -0700

    replace example inputs with a placeholder input line. new item appends to screen using state.

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider } from '@ionic/react';
 
-import InputList from '../components/InputList';
+
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -12,57 +12,33 @@ const Tab1: React.FC = () => {
     <IonPage>
         <IonHeader>
           <IonToolbar>
-            <IonTitle>IonInput Examples</IonTitle>
+            <IonTitle>Enter items needed below</IonTitle>
           </IonToolbar>
         </IonHeader>
         <IonContent>
           <IonList>
-            <IonItemDivider>Default Input with Placeholder</IonItemDivider>
-            <IonItem>
-              <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)}></IonInput>
-            </IonItem>
 
-            <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
-            <IonItem>
-              <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)} clearInput></IonInput>
-            </IonItem>
 
-            <IonItemDivider>Number type input</IonItemDivider>
-            <IonItem>
-              <IonInput type="number" value={number} placeholder="Enter Number" onIonChange={e => setNumber(parseInt(e.detail.value!, 10))}></IonInput>
-            </IonItem>
-
-            <IonItemDivider>Disabled input</IonItemDivider>
-            <IonItem>
-              <IonInput value={text} disabled></IonInput>
-            </IonItem>
-
-            <IonItemDivider>Readonly input</IonItemDivider>
-            <IonItem>
-              <IonInput value={text} readonly></IonInput>
-            </IonItem>
-
-            <IonItemDivider>Inputs with labels</IonItemDivider>
 
             <IonItem>
-              <IonLabel>Default Label</IonLabel>
-              <IonInput></IonInput>
+
+              <IonInput value={text}
+              placeholder="new item"
+             onIonChange={e => setText(e.detail.value!)} clearInput ></IonInput>
             </IonItem>
 
-            <IonItem>
-              <IonLabel position="floating">Floating Label</IonLabel>
-              <IonInput value={text}></IonInput>
-            </IonItem>
+
+
+
+
+            <IonItemDivider/>
 
             <IonItem>
-              <IonLabel position="fixed">Fixed Label</IonLabel>
-              <IonInput value={text}></IonInput>
+                {text}
             </IonItem>
 
-            <IonItem>
-              <IonLabel position="stacked">Stacked Label</IonLabel>
-              <IonInput value={text}> </IonInput>
-            </IonItem>
+
+
           </IonList>
         </IonContent>
       </IonPage>
